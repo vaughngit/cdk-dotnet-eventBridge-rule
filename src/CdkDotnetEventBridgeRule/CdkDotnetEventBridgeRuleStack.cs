ce6651ec1863a13c0ea824793d9b940173780b2e
awsalvin <awsalvin@amazon.com>
2023-08-14 14:26:40 -0500

    initial commit

--- src/CdkDotnetEventBridgeRule/CdkDotnetEventBridgeRuleStack.cs
+++ src/CdkDotnetEventBridgeRule/CdkDotnetEventBridgeRuleStack.cs
@@ -1,5 +1,10 @@
+using System.Text.Json;
 using Amazon.CDK;
+using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.Events;
+using Amazon.CDK.AWS.SQS;
 using Constructs;
+
 
 namespace CdkDotnetEventBridgeRule
 {
@@ -7,7 +12,61 @@ namespace CdkDotnetEventBridgeRule
     {
         internal CdkDotnetEventBridgeRuleStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
+            var vpc = new Vpc(this, "Vpc", new VpcProps
+            {
+                NatGateways = 0
+            });
+
+            // create the dead letter queue
+            var deadLetterQueue = new Queue(this, "DeadLetterQueueName", new QueueProps
+            {
+                QueueName = "DeadLetterQueueName",
+                DeliveryDelay = Duration.Millis(0),
+                RetentionPeriod = Duration.Days(14),
+                Encryption = QueueEncryption.UNENCRYPTED
+            });
+
+            //create the primary queue
+            var serviceQueue = new Queue(this, "ServiceQueueName", new QueueProps
+            {
+                QueueName = "ServiceQueueName",
+                DeliveryDelay = Duration.Millis(0),
+                ReceiveMessageWaitTime = Duration.Seconds(20),
+                VisibilityTimeout = Duration.Minutes(15),
+                RetentionPeriod = Duration.Days(14),
+                DeadLetterQueue = new DeadLetterQueue
+                {
+                    MaxReceiveCount = 4,
+                    Queue = deadLetterQueue
+                },
+                Encryption = QueueEncryption.UNENCRYPTED
+            });
+
             // The code that defines your stack goes here
+            var eventBus = new EventBus(this, "Bus");
+
+            var ruleTarget = new Amazon.CDK.AWS.Events.Targets.SqsQueue(serviceQueue);
+
+            var detailType = new string[] { "replaceme" };
+            var eventPattern = new EventPattern() { DetailType = detailType };
+
+            var rule = new Rule(this, "EventRuleName", new RuleProps
+            {
+                EventBus = eventBus,
+                Enabled = true,
+                RuleName = "EventRuleName",
+                Targets = new[] { ruleTarget },
+                EventPattern = eventPattern
+            });
+
+            var myCfnRule = (Amazon.CDK.AWS.Events.CfnRule)rule.Node.DefaultChild;
+
+            var mailchip = new System.Collections.Generic.Dictionary<string, string>() { { "prefix", "mailchimp:" } };
+            var detailtype = new object[] { mailchip };
+            var eventpattern = new System.Collections.Generic.Dictionary<string, object>() { { "detail-type", detailtype } };
+
+            myCfnRule.EventPattern = eventpattern;
+
         }
     }
 }
--- src/CdkDotnetEventBridgeRule/GlobalSuppressions.cs
+++ src/CdkDotnetEventBridgeRule/GlobalSuppressions.cs
@@ -1,1 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
+
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0026:Possible unassigned object created by 'new'", Justification = "Constructs add themselves to the scope in which they are created")]
+[assembly: SuppressMessage("Performance", "CA1806:Do not ignore method results", Justification = "<Pending>", Scope = "member", Target = "~M:CdkDotnetEventBridgeRule.Program.Main(System.String[])")]
